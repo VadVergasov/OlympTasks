492e7248b79a4f27530b7839d1163cbed36f58d9
Vadim Vergasov <vadim.vergasov2003@gmail.com>
2019-02-22 13:07:17 +0300

    Restructured DL folder

--- DL/CIty/2013/Day 1/1108309/main.cpp
+++ DL/CIty/2013/Day 1/1108309/main.cpp
@@ -13,7 +13,6 @@ int main() {
     for (int i = 0; i < 2 * n; i++) {
         in >> hours >> a >> minutes >> b >> sec;
         c.insert(hours * 3600 + minutes * 60 + sec);
-        548
     }
     for (int i = 0; i < n; i++) {
         cout << c[2 * i - 1] / 3600 << ":" <<
