b0c0588d181d88276ca12360f2226e08c728f25b
Vadim Vergasov <vadim.vergasov2003@gmail.com>
2021-03-06 12:19:41 +0300

    Fixed license in cpp files

--- CodeForces/1117/C/main.cpp
+++ CodeForces/1117/C/main.cpp
@@ -1,95 +1,12 @@
-"""
-Main code for this task.
-Copyright (C) 2021  Vadim Vergasov aka VadVergasov
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>.
-"""
-#include <bits/stdc++.h>
-
-using namespace std;
-
-int main() {
-    int x1, x2, y1, y2, n, xv = 0, yv = 0, xdif, ydif, xcur, ycur, res = 0;
-    cin >> x1 >> y1 >> x2 >> y2 >> n;
-    xcur = x1;
-    ycur = y1;
-    xdif = x1 - x2;
-    ydif = y1 - y2;
-    string s;
-    cin >> s;
-    for (int i = 0; i < n && (xcur != x2 || ycur != y2); i++) {
-        res++;
-        if (s[i] == 'U') {
-            yv++;
-            if (y2 - ycur > 0) {
-                if (abs(ycur - y2) > abs(xcur - x2)) {
-                    ycur += 2;
-                } else {
-                    ycur++;
-                    if (x2 - xcur > 0) {
-                        xcur++;
-                    } else {
-                        xcur--;
-                    }
-                }
-            }
-        } else if (s[i] == 'D') {
-            yv--;
-            if (y2 - ycur < 0) {
-                if (abs(ycur - y2) > abs(xcur - x2)) {
-                    ycur -= 2;
-                } else {
-                    ycur--;
-                    if (x2 - xcur > 0) {
-                        xcur++;
-                    } else {
-                        xcur--;
-                    }
-                }
-            }
-        } else if (s[i] == 'R') {
-            xv++;
-            if (x2 - xcur > 0) {
-                if (abs(xcur - x2) > abs(ycur - y2)) {
-                    xcur += 2;
-                } else {
-                    xcur++;
-                    if (y2 - ycur > 0) {
-                        ycur++;
-                    } else {
-                        ycur--;
-                    }
-                }
-            }
-        } else {
-            xv--;
-            if (x2 - xcur < 0) {
-                if (abs(xcur - x2) > abs(ycur - y2)) {
-                    xcur -= 2;
-                } else {
-                    xcur--;
-                    if (y2 - ycur > 0) {
-                        ycur++;
-                    } else {
-                        ycur--;
-                    }
-                }
-            }
-        }
-    }
-    cout << xcur << " " << ycur << "\n";
-    if ((xv + yv) * -1 == n) {
-        cout << -1;
-        return 0;
-    }
-    cout << res;
-    return 0;
-}
+// Main code for this task.
+// Copyright (C) 2021  Vadim Vergasov aka VadVergasov
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
--- CodeForces/985/B/main.cpp
+++ CodeForces/985/B/main.cpp
@@ -1,75 +1,12 @@
-"""
-Main code for this task.
-Copyright (C) 2021  Vadim Vergasov aka VadVergasov
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>.
-"""
-#include <bits/stdc++.h>
-
-using namespace std;
-
-int main() {
-    int n, m;
-    string tmp;
-    cin >> n >> m;
-    vector<string> a;
-    for (int i = 0; i < n; i++) {
-        cin >> tmp;
-        a.push_back(tmp);
-    }
-    vector<int> sum;
-    sum.resize(m, 0);
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            sum[j] += a[i][j] - '0';
-        }
-    }
-    if (n == 1) {
-        cout << "NO";
-        return 0;
-    }
-    string mask;
-    bool res = true;
-    for (int i = 0; i < m; i++) {
-        if (sum[i] == 0) {
-            res = false;
-            break;
-        }
-        if (sum[i] == 1) {
-            mask += '0';
-        } else {
-            mask += '1';
-        }
-    }
-    if (!res) {
-        cout << "NO";
-        return 0;
-    }
-    res = false;
-    for (int i = 0; i < n; i++) {
-        int t = 0;
-        for (int j = 0; j < m; j++) {
-            if (a[i][j] == mask[j] || mask[j] == '1' && a[i][j] == '0') {
-                t++;
-            }
-        }
-        if (t == m) {
-            res = true;
-            break;
-        }
-    }
-    if (res) {
-        cout << "YES";
-    } else {
-        cout << "NO";
-    }
-    return 0;
-}
+// Main code for this task.
+// Copyright (C) 2021  Vadim Vergasov aka VadVergasov
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
