abdf444c6cb6f1d7458b028ed0fe86ba2728add8
Vadzim Verhasau <vadim.vergasov2003@gmail.com>
2023-01-26 20:50:26 +0300

    Added few sort me tasks

---
+++ Sort-Me/3/main.cpp
@@ -0,0 +1,40 @@
+// Main code for this task.
+// Copyright (C) 2022  Vadzim Verhasau aka VadVergasov
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    long long n, k, res = 0;
+    cin >> n >> k;
+    multiset<long long> a;
+    set<long long> b;
+    for (int i = 0; i < n; i++) {
+        int tmp;
+        cin >> tmp;
+        a.insert(tmp);
+        b.insert(tmp);
+    }
+    for (const auto& i : b) {
+        if (b.find(k - i) == b.end() || a.find(i) == a.end()) {
+            continue;
+        }
+        res += min(a.count(i), a.count(k - i));
+        a.erase(k - i);
+    }
+    cout << res;
+    return 0;
+}
